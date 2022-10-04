0688749c94028cc218d1deae8020902b99fe94a2
flammableassassin <43076116+flamableassassin@users.noreply.github.com>
2022-10-04 10:43:06 +0100

    fixing invaild data not being ignored

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ def main():
     file.write(output)
 
 
+ignoreRows = [0, 1, 2, 3]
+usedMonths = ["jan", "feb", "mar", "apr", "may",
+              "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+
+
 def parseTable(inputTable):
     file = open("./data/" + inputTable, "r")
     rows = file.readlines()
@@ -28,32 +33,44 @@ def parseTable(inputTable):
 
     rows.pop(len(rows) - 1)  # Removing the National Figures row
 
-    ignoreRows = [0, 1, 2, 3]
-
     currentLocation = ""
 
     for i in range(len(rows)):
         if (i in ignoreRows):  # Ignoring the few rows
             continue
-        row = rows[i]
+        row = rows[i].replace("\n", "")
 
-        items = row.split(",")  # Splitting the row into items
+        cells = row.split(",")  # Splitting the row into cells
 
-        if (items[1] == ""):
-            currentLocation = items[0]
+        if (cells[0] == "Jan-Mar 2021"):  # Ignoring combined months
+            continue
+
+        if ("Total tests" in cells):
+            continue
+
+        if (cells[1] == "" and not checkMonth(cells[0])):  # If the row is a month row
+            currentLocation = cells[0]
             continue  # If the row has only one item, it is the start of a location
 
-        if (items[0] == currentLocation):
+        if (cells[0] == currentLocation):
             continue  # If the row is not empty and the first item is the location, it the total for the location
-        date = items.pop(0)  # Removing the date from the items
+        date = cells.pop(0)  # Removing the date from the cells
 
-        # Removing the last item which is the new line character
-        items.pop(len(items) - 1)
-        # Removing empty items
-        items = list(filter(lambda item: item != " ", items))
+        # Removing empty cells
+        cells = list(filter(lambda item: item != " " and item != "", cells))
 
-        table.append(",".join([date, currentLocation, *items]))
+        table.append(",".join([date, currentLocation, *cells]))
     return table
+
+
+def checkMonth(cell):
+    #  month = cells[0].lower().split("-")[0]
+    found = False
+    for month in usedMonths:
+        if (cell.lower().startswith(month)):
+            found = True
+            break
+    return found
 
 
 main()
